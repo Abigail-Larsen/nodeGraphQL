409a186a21160b88075472b3c9749150c09d2499
Abigail <abigailthelin@workfront.com>
2021-03-18 16:30:23 -0600

    update

--- src/router.js
+++ src/router.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs')
 var request = require('request-promise'); 
+var sql = require('mssql')
 
-router.get('/health', function (req, res) {
-    console.log("HEY THERE")
-    res.send('HEALTHY')
-});
+// router.get('/health', function (req, res) {
+//     console.log("HEY THERE")
+//     res.send('HEALTHY')
+// });
 
 router.post('/createNewVote', function (req, res) {
     // send to db
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ var path = require('path');
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var cors = require('cors')
+// var { Pool } = require('pg');
+
 var router = require('./router')
 var schema = require('./schema')
 var root = require('./root')
@@ -14,12 +16,28 @@ app.use(
   express.urlencoded({
     extended: true
   })
-  );
+);
   
 app.use(express.json());
 app.use(router)
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: false
+// });
+router.get('/health', async (req, res) => {   
+  // try {
+  //   const client = await pool.connect();
+  //   const result = await client.query('SELECT * FROM test_table');
+  //   // const results = { 'results': (result) ? result.rows : null};
+  //   // res.render('pages/db', results );
+  //   // client.release();
+  // } catch (err) {
+  //   console.error(err);
+  //   res.send("Error " + err);
+  // }
+});
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
