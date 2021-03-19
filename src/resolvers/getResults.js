ac68f755a6b7e43b35c2725039cf07ba9c5590e0
Abigail <abigailthelin@workfront.com>
2021-03-19 14:04:32 -0600

    fix

--- src/resolvers/getResults.js
+++ src/resolvers/getResults.js
@@ -8,12 +8,10 @@ const pool = new Pool({
 });
 
 const getResults = async (req, res) => {
-    console.log("heyyy", res.body.variables)
     const client = await pool.connect();
     const queryString = `SELECT * FROM votesDB WHERE id='${res.body.variables.id}'`;
     const result = await client.query(queryString);
     client.release();
-    console.log('result', result.rows)
     return result.rows
 }
 
